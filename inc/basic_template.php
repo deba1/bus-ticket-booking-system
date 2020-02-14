c385b5dc44517cb9d65cfbb76433a0e9f6e4c222
deba1 <dsarker333@gmail.com>
2020-02-14 23:46:01 +0600

    Migrate to CDN

--- inc/basic_template.php
+++ inc/basic_template.php
@@ -8,8 +8,8 @@ function t_header($title) {
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
   <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css"/>
   <link rel="stylesheet" type="text/css" href="css/style.css?'.date('his').'"/>
-  <script type="text/javascript" src="js/jquery.min.js"></script>
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>';
 }
